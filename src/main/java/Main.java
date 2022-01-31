f7521e96994e69195db0c1fdc8daf2677c5a28f1
BSmithMarin <brahyansmithm@gmail.com>
2022-01-31 21:42:27 +0100

    Creada funcionalidad para obtener sessionFactory con singleton

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import util.HibernateUtil;
+
 public class Main {
     public static void main(String[] args) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        Transaction tax = session.beginTransaction();
+
+        try{
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            session.close();
+            if(tax.isActive())
+                tax.rollback();
+        }
 
     }
 }
---
+++ src/main/java/util/HibernateUtil.java
@@ -0,0 +1,27 @@
+package util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static SessionFactory sf ;
+
+    private HibernateUtil(){}
+
+    public static SessionFactory getSessionFactory(){
+
+        //Creacion del session factory a partir del fichero hibernate.cfg.xml
+        if (sf == null){
+            StandardServiceRegistry sr = new StandardServiceRegistryBuilder().configure().build();
+            sf = new MetadataSources(sr).buildMetadata().buildSessionFactory();
+            
+        }
+
+        return sf;
+    }
+
+}
